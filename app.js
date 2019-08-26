0efa049dd600c519cbb9c5fc4bfb4f3512958a72
Adam Pierce <adam.pierc3@gmail.com>
2019-08-26 12:06:04 -0700

    added file upload functionality for posts

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const uuidv4 = require('uuid/v4');
+const multer = require('multer');
 
 const PRIVATE = require('./util/database.priv.js');
 const feedRoutes = require('./routes/feed');
@@ -11,8 +13,27 @@ const app = express();
 
 const MONGODB_URI = 'mongodb+srv://node:' +PRIVATE.MONGO_PASSWORD +'@online-shop-dkmzb.mongodb.net/messages?w=majority';
 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        let extension = file.originalname.split('.').pop();
+        cb(null, uuidv4() +'.' +extension)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(multer({storage: storage, fileFilter: fileFilter}).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
