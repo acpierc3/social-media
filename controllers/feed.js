763fe218c0a478496c27430066a0938c94c33ba4
Adam Pierce <adam.pierc3@gmail.com>
2019-08-26 12:43:58 -0700

    added functionality to be able to update posts and images

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const {validationResult} = require('express-validator');
 
 const Post = require('../models/post');
@@ -70,4 +73,54 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     })
+}
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect');
+    error.statusCode = 422;
+    throw error;
+  }
+  let imageUrl = req.body.image;
+  if(req.file) {
+    console.log('NEW IMAGE DETECTED');
+    imageUrl = req.file.path.replace("\\", "/");
+  }
+  if(!imageUrl) {
+    const error = new Error('No image picked.');
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(req.params.postId)
+    .then(post => {
+      if(!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+      //if the new image url is different, delete the old one
+      if(imageUrl !== post.imageUrl) {
+        console.log('DELETING OLD IMAGE')
+        clearImage(post.imageUrl);
+      }
+      post.title = req.body.title;
+      post.imageUrl = imageUrl;
+      post.content = req.body.content;
+      return post.save()
+    })
+    .then(result => {
+      res.status(200).json({post: result});
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    })
+}
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 }
