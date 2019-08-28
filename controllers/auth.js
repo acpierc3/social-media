96086d4f868f1e9053b48bc98e9f041d08bafafb
Adam Pierce <adam.pierc3@gmail.com>
2019-08-28 13:33:49 -0400

    added login route and user tokens

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
 const {validationResult} = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const PRIVATE = require('../util/database.priv.js');
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
@@ -32,5 +34,44 @@ exports.signup = (req, res, next) => {
             }
             next(err);
         })
-
 }
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+
+    User.findOne({email: email})
+        .then(user => {
+            if(!user) {
+                const error = new Error('User not found');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if(!isEqual){
+                const error = new Error('Wrong password');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign(
+                {
+                    email: loadedUser.email,
+                    userId: loadedUser._id.toString()
+                }, 
+                PRIVATE.JWT_PRIVATE_KEY, 
+                {expiresIn: '1h'}
+            );
+            res.status(200).json({token: token, userId: loadedUser._id.toString()})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
