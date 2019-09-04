3fe9303ae7c2fcfec4e1774ada5b4a816503b1ba
Adam Pierce <adam.pierc3@gmail.com>
2019-09-04 11:09:11 -0700

    added controller actions for web socket updates when deleting or editing posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -14,6 +14,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
         .populate('creator')
+        .sort({createdAt: -1})
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     res.status(200).json({posts: posts, totalItems: totalItems});
@@ -53,6 +54,8 @@ exports.createPost = async (req, res, next) => {
     user.posts.push(post);
     await user.save();
     //after we are done creating post, inform other clients about the new post via web sockets
+    //emit() sends info to ALL clients, including the one who created the new post
+    //so in the front end logic, i removed the original logic to add post, since post will be added by web socket logic
     //emit() automatically converts object to JSON
     io.getIO().emit('posts', {action: 'create', post: {...post._doc, creator: {_id: req.userId, name: user.name}}})
     res.status(201).json({
@@ -105,13 +108,13 @@ exports.updatePost = async (req, res, next) => {
     throw error;
   }
   try {
-    const post = await Post.findById(req.params.postId);
+    const post = await Post.findById(req.params.postId).populate('creator');
     if(!post) {
       const error = new Error('Could not find post');
       error.statusCode = 404;
       throw error;
     }
-    if(post.creator.toString() !== req.userId.toString()) {
+    if(post.creator._id.toString() !== req.userId.toString()) {
       const error = new Error('Not Authorized');
       error.statusCode = 403;
       throw error;
@@ -124,6 +127,7 @@ exports.updatePost = async (req, res, next) => {
     post.imageUrl = imageUrl;
     post.content = req.body.content;
     const result = await post.save();
+    io.getIO().emit('posts', {action: 'update', post: result})
     res.status(200).json({post: result});
     
   } catch (err) {
@@ -152,6 +156,7 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.pull(req.params.postId);
     await user.save();
+    io.getIO().emit('posts', {action: 'delete', post: req.params.postId})
     res.status(200).json({message: "Post deleted"});
   } catch(err) {
     if(!err.statusCode) {
