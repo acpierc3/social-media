fa3273b69dae40d8b2f3871d1ec8b8181d811256
Adam Pierce <adam.pierc3@gmail.com>
2019-09-05 11:21:38 -0700

    adding validation to graphql query

--- app.js
+++ app.js
@@ -43,13 +43,27 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    //this code is needed because OPTIONS requests are not allowed in graphQL
+    //so otherwise they would cause an error once reaching graphql middleware
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
 app.use('/graphql', graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occurred.';
+        const code = err.originalError.code || 500;
+        return {message: message, status: code, data: data}
+    }
 }));
 
 app.use((error, req, res, next) => {
