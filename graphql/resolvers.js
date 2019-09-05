04e120b67cbe2f34cc8cbc3ec4fb78c2eaa4ce68
Adam Pierce <adam.pierc3@gmail.com>
2019-09-05 11:56:04 -0700

    adding login action to schema and resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,10 @@
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+
+const PRIVATE = require('../util/database.priv.js');
 
 module.exports = {
     //args data contains data defined in schema user input
@@ -40,5 +43,31 @@ module.exports = {
         //._doc returns user object without excess metadata
         //then need to overwrite _id so it is a string, not an object
         return {...createdUser._doc, _id: createdUser._id.toString()}
+    },
+
+    //destructured email and password args for simplicity
+    login: async function({ email, password }, req) {
+
+        const user = await User.findOne({email: email})
+        if(!user) {
+            const error = new Error('User not found');
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if(!isEqual){
+            const error = new Error('Incorrect password');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = jwt.sign(
+            {
+                email: user.email,
+                userId: user._id.toString()
+            }, 
+            PRIVATE.JWT_PRIVATE_KEY, 
+            {expiresIn: '1h'}
+        );
+        return {token: token, userId: user._id.toString()};
     }
 };
