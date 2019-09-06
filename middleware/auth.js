5d6448a8cc48a68543d83af68674fed9517fb43b
Adam Pierce <adam.pierc3@gmail.com>
2019-09-06 11:12:13 -0700

    adding auth logic to graphql

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,23 +6,22 @@ module.exports = (req, res, next) => {
     //get token from request header
     const authHeader = req.get('Authorization');
     if(!authHeader) {
-        const error = new Error('Not authenticated.');
-        err.statusCode = 401;
-        throw error;
+        req.isAuth = false;
+        return next();
     }
     const token = authHeader.split(' ')[1];
     let decodedToken;
     try {
         decodedToken = jwt.verify(token, PRIVATE.JWT_PRIVATE_KEY);
     } catch(err) {
-        err.statusCode = 500;
-        throw err;
+        req.isAuth = false;
+        return next();
     }
     if(!decodedToken) {
-        const error = new Error('Not authenticated.');
-        err.statusCode = 401;
-        throw error;
+        req.isAuth = false;
+        return next();
     }
     req.userId = decodedToken.userId;
+    req.isAuth = true;
     next();
 }
