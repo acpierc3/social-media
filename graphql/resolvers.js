d06297a25fa43993bd9e16359067e3672331ac6d
Adam Pierce <adam.pierc3@gmail.com>
2019-09-06 10:11:27 -0700

    adding create post logic to graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -69,5 +69,22 @@ module.exports = {
             {expiresIn: '1h'}
         );
         return {token: token, userId: user._id.toString()};
+    },
+
+    createPost: async function({ postInput }, req) {
+        const errors = [];
+
+        if(validator.isEmpty(postInput.title) || !validator.isLength(postInput.title), {min: 5}) {
+            errors.push({message: 'Title is invalid.'});
+        }
+        if(validator.isEmpty(postInput.content) || !validator.isLength(postInput.content), {min: 5}) {
+            errors.push({message: 'Content is invalid.'});
+        }
+        if(errors.length > 0) {
+            const error = new Error('Invalid input');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
     }
 };
